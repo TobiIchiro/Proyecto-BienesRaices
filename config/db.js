da8c5931ccb50ccc3b8ffad1b40624b656ebb8e6
Israel <israel.toledo@outlook.es>
2024-08-07 14:45:40 -0600

    Conexión a base de datos

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize"
+
+const db = new Sequelize(
+    'bienesraices',
+    'root',
+    'root',
+    {
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
+        define: {
+            timestamp: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        operatorAliases: false
+    }
+);
+
+export default db
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express'
 import userRoutes from './Routes/userRoutes.js'
+import db from './config/db.js'
 //Crear la app
 const app = express()
+
+//Conexión a la base de datos
+try
+{
+    await db.authenticate();
+    console.log('Conexión correcta a la base de datos')
+}
+catch(error)
+{
+    console.log(error)
+}
 
 //Habilitar pug
 app.set('view engine', 'pug')
