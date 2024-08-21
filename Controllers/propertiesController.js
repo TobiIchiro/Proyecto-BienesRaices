8183d82882079cf3711b713cce576c893f8f52a3
Israel <israel.toledo@outlook.es>
2024-08-21 14:45:10 -0600

    Creacion de middleware, lectura y validacion de JWT, Eliminación de password de la respuesta de la consulta, Agregó el Usuario Autenticado a la Propiedad Creada

--- Controllers/propertiesController.js
+++ Controllers/propertiesController.js
@@ -1,10 +1,9 @@
-import Price from '../models/Price.js'
-import Category from '../models/Category.js'
+import {validationResult} from 'express-validator'
+import {Price, Category, Property} from '../models/index.js'
 
 const admin = (req,res) => {
     res.render('./properties/admin.pug',{
-        pagina : 'Mis propiedades',
-        bar : true
+        pagina : 'Mis propiedades'
     })
 }
 
@@ -17,12 +16,68 @@ const add = async (req, res) => {
 
     res.render('./properties/add.pug',{
         pagina : 'Crear propiedad',
-        bar : true,
+        csrfToken : req.csrfToken(),
         categories: categories,
-        prices: prices
+        prices: prices,
+        datos: {}
     })
 }
 
-export {admin,
-    add
+const save = async (req, res) => {
+    //Validación
+    let result = validationResult(req)
+    if(!result.isEmpty()) {
+        const [categories, prices] = await Promise.all([
+            Category.findAll(),
+            Price.findAll()
+        ])
+        return res.render('./properties/add.pug',{
+            pagina : 'Crear propiedad',
+            csrfToken : req.csrfToken(),
+            categories: categories,
+            prices: prices,
+            errors: result.array(),
+            datos: req.body
+        })
+    }
+    
+    //Crear un resgistro
+    const {title, description, rooms, parking, wc, street, lat, lng, price, category: categoryId} = req.body
+    const {id: userId} = req.user
+
+    try{
+        const propertySaved = await Property.create({
+            title,
+            description,
+            rooms,
+            parking,
+            wc,
+            street,
+            lat,
+            lng,
+            priceId : price,
+            categoryId,
+            userId,
+            imagen: ''
+        })
+        const {id} = propertySaved
+        res.redirect(`/my-properties/add-image/${id}`)
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+
+const addImage = (req, res) => {
+    res.render('properties/add-image.pug', {
+        pagina: 'Agregar imagenes'
+    })
+}
+
+export {
+    admin,
+    add,
+    save,
+    addImage
 }
--- Routes/propertiesRoutes.js
+++ Routes/propertiesRoutes.js
@@ -1,12 +1,31 @@
 import express from 'express'
+import { body } from 'express-validator'
 
-import {admin,
-    add
+import {
+    admin,
+    add,
+    save,
+    addImage
 } from '../Controllers/propertiesController.js'
+
+import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.get('/my-properties',admin)
-router.get('/my-properties/add', add)
+router.get('/my-properties',protectRoute, admin)
 
+router.get('/my-properties/add',protectRoute, add)
+router.post('/my-properties/add',protectRoute,
+    body('title').notEmpty().withMessage('Es necesario el título del anuncio'),
+    body('description')
+        .notEmpty().withMessage('Es necesaria el la descripción de la propiedad')
+        .isLength({ max: 200}).withMessage('La descripción es muy larga'),
+    body('category').isNumeric().withMessage('Selecciona una categoría'),
+    body('price').isNumeric().withMessage('Selecciona una rango de precios'),
+    body('rooms').isNumeric().withMessage('Selecciona un numero de habitaciones'),
+    body('parking').isNumeric().withMessage('Selecciona un numero de estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona un numero de baños'),
+    body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
+    save)
+router.get('/my-properties/add-image/:id',addImage)
 export default router
